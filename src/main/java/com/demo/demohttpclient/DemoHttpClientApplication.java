0f6e23913849f1dfc5858f3943dd68fd2872025d
yusanghuck <g01099387636@gmail.com>
2022-09-21 16:03:54 +0900

    init

---
+++ src/main/java/com/demo/demohttpclient/DemoHttpClientApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demohttpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHttpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHttpClientApplication.class, args);
+    }
+
+}
